b6aa538e484e996feb53bbb23d64b12d670527b3
Julie J <jeannet.julie@gmail.com>
2022-07-24 16:18:39 +0200

    btn-back-to-top

--- Code_integration-template_BS5/main.js
+++ Code_integration-template_BS5/main.js
@@ -1,40 +1,40 @@
 (function() {
     
-    "use strict";
-    
-        
-    /*=====================================
-    Sticky pour faire en sorte que la navbar change de background au scroll de la page
-    ======================================= */
-    window.onscroll = function () {
-        var navbar = document.getElementById("navbar");
-        var nav = document.querySelector("nav");
-        var sticky = navbar.offsetTop;
+  "use strict";
+  
+      
+  /*=====================================
+  Sticky pour faire en sorte que la navbar change de background au scroll de la page
+  ======================================= */
+  window.onscroll = function () {
+      var navbar = document.getElementById("navbar");
+      var nav = document.querySelector("nav");
+      var sticky = navbar.offsetTop;
 
-        if (window.pageYOffset > sticky) {
-            nav.classList.add('bg-light', 'navbar-light');
-            nav.classList.remove("navbar-dark");
-        } else {
-            nav.classList.remove("bg-light", "navbar-light");
-            nav.classList.add("navbar-dark");
-        }
+      if (window.pageYOffset > sticky) {
+          nav.classList.add('bg-light', 'navbar-light');
+          nav.classList.remove("navbar-dark");
+      } else {
+          nav.classList.remove("bg-light", "navbar-light");
+          nav.classList.add("navbar-dark");
+      }
 
-        // show or hide the back-top-top button
-        var backTo = document.querySelector(".back-to-top");
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            backTo.style.display = "block";
-        } else {
-            backTo.style.display = "none";
-        }
-    };
+      // show or hide the back-top-top button
+      var backTo = document.querySelector("#btn-back-to-top");
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+          backTo.style.display = "block";
+      } else {
+          backTo.style.display = "none";
+      }
+  };
 
- 
-    document.querySelectorAll(".nav-link").forEach(e =>
-        e.addEventListener("click", () => {
-            navbarToggler.classList.remove("active");
-            navbarCollapse.classList.remove('show')
-        })
-    );
-    
-    
+
+  document.querySelectorAll(".nav-link").forEach(e =>
+      e.addEventListener("click", () => {
+          navbarToggler.classList.remove("active");
+          navbarCollapse.classList.remove('show')
+      })
+  );
+  
+  
 })();
