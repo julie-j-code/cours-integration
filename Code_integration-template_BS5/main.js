1f440256fdc4a2262421d02d3e02e1a31c2f8dfe
Julie J <jeannet.julie@gmail.com>
2022-07-24 14:41:41 +0200

    back à revoir

--- Code_integration-template_BS5/main.js
+++ Code_integration-template_BS5/main.js
@@ -20,11 +20,11 @@
         }
 
         // show or hide the back-top-top button
-        var backToTo = document.querySelector(".back-to-top");
+        var backTo = document.querySelector(".back-to-top");
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            backToTo.style.display = "block";
+            backTo.style.display = "block";
         } else {
-            backToTo.style.display = "none";
+            backTo.style.display = "none";
         }
     };
 
