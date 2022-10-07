5e9996fc04c6ae8798e0440d2062b84ce5f9cebb
Julie J <jeannet.julie@gmail.com>
2022-10-07 07:48:35 +0200

    Ajout fonction changeColor

--- Exemples_variables_css_js/second/assets/js/main.js
+++ Exemples_variables_css_js/second/assets/js/main.js
@@ -15,10 +15,19 @@ showMenu('nav-toggle','nav-menu')
 
 /*----- Changement des couleurs -----*/
 const sizes = document.querySelectorAll('.size_content span');
+const colors = document.querySelectorAll('.sneaker_color');
+const sneaker = document.querySelectorAll('.sneaker img');
 
 function changeSize(){
     sizes.forEach(size => size.classList.remove('active'));
     this.classList.add('active');
 }
-sizes.forEach(size => size.addEventListener('click', changeSize));
 
+function changeColor(){
+    colors.forEach(c => c.classList.toggle('active'));
+    sneaker.forEach(s => s.classList.toggle('shows'));
+
+}
+
+sizes.forEach(size => size.addEventListener('click', changeSize));
+colors.forEach(c => c.addEventListener('click', changeColor));
